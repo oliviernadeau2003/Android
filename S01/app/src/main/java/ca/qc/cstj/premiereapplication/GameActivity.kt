1ae86a032e100cc909bbc05f8512f2992a4a04c5
Olivier Nadeau <oliviernadeau2003@gmail.com>
2023-01-26 11:53:52 -0500

    End Of Class

---
+++ S01/app/src/main/java/ca/qc/cstj/premiereapplication/GameActivity.kt
@@ -0,0 +1,22 @@
+package ca.qc.cstj.premiereapplication
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.reflect.typeOf
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            val intent = Intent(context, GameActivity::class.java)
+            return intent
+        }
+    }
+
+}
---
+++ S01/app/src/main/java/ca/qc/cstj/premiereapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package ca.qc.cstj.premiereapplication
+
+import android.app.appsearch.AppSearchBatchResult
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPhone = findViewById<Button>(R.id.btnPhone)
+        val btnSMS = findViewById<Button>(R.id.btnSMS)
+        val btnGame = findViewById<Button>(R.id.btnGame)
+
+        btnPhone.setOnClickListener {
+            //Toast.makeText(this, R.string.msgHello, Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:4508529614"))
+            startActivity(intent)
+        }
+
+        btnSMS.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("smsto:4504361580"))
+            intent.putExtra("sms_body", "Message de mon code d'amour")
+            startActivity(intent);
+        }
+
+        btnGame.setOnClickListener {
+            startActivity(GameActivity.newIntent(this))
+        }
+    }
+}
