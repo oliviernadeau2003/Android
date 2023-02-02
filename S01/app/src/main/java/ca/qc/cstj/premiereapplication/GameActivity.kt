c8c1d6dc15bbad4cea60143a22960538383c91f0
Olivier Nadeau <oliviernadeau2003@gmail.com>
2023-02-02 09:11:44 -0500

    End Of Project

--- S01/app/src/main/java/ca/qc/cstj/premiereapplication/GameActivity.kt
+++ S01/app/src/main/java/ca/qc/cstj/premiereapplication/GameActivity.kt
@@ -4,19 +4,76 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlin.reflect.typeOf
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class GameActivity : AppCompatActivity() {
+
+    private var nbTries = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val txvWelcomePhrase = findViewById<TextView>(R.id.txvWelcomePhrase)
+        val txvTries = findViewById<TextView>(R.id.txvTries)
+        val nprNumber = findViewById<NumberPicker>(R.id.nprNumber)
+        val btnValidate = findViewById<Button>(R.id.btnValidate)
+
+        txvWelcomePhrase.text =
+            getString(R.string.msg_Game, intent.getStringExtra(PLAYER_NAME_EXTRA))
+
+        nprNumber.minValue = 0
+        nprNumber.maxValue = 100
+
+        val theGoodNumber = (0..100).random()
+
+        btnValidate.setOnClickListener {
+            nbTries++
+            val choice = nprNumber.value
+
+//            if (choice == theGoodNumber) {
+//                Snackbar.make(btnValidate, "Bravo", Snackbar.LENGTH_INDEFINITE).show()
+//            } else if (choice > theGoodNumber) {
+//                Snackbar.make(btnValidate, "Le nombre recherché est plus petit", Snackbar.LENGTH_LONG).show()
+//            } else {
+//                Snackbar.make(btnValidate, "Le nombre recherché est plus grand", Snackbar.LENGTH_LONG).show()
+//            }
+
+            when{
+                choice == theGoodNumber -> {
+                    Snackbar.make(btnValidate, "Bravo", Snackbar.LENGTH_INDEFINITE).show()
+                    finish()
+                }
+                choice > theGoodNumber -> {
+                    Snackbar.make(btnValidate, "Le nombre recherché est plus petit", Snackbar.LENGTH_LONG).show()
+
+                } else -> {
+                    Snackbar.make(btnValidate, "Le nombre recherché est plus grand", Snackbar.LENGTH_LONG).show()
+                }
+            }
+
+
+
+            txvTries.text = applicationContext.resources.getQuantityString(R.plurals.nbTries,nbTries,nbTries)
+
+        }
+
+
     }
 
+    //  La partie static Intent newIntent()
     companion object {
-        fun newIntent(context: Context): Intent {
+        const val PLAYER_NAME_EXTRA = "PLAYER_NAME"
+        fun newIntent(context: Context, playerName: String): Intent {
             val intent = Intent(context, GameActivity::class.java)
+            intent.putExtra(PLAYER_NAME_EXTRA, playerName)
             return intent
         }
     }
+
 
 }
--- S01/app/src/main/java/ca/qc/cstj/premiereapplication/MainActivity.kt
+++ S01/app/src/main/java/ca/qc/cstj/premiereapplication/MainActivity.kt
@@ -1,13 +1,11 @@
 package ca.qc.cstj.premiereapplication
 
-import android.app.appsearch.AppSearchBatchResult
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import java.net.URL
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         val btnPhone = findViewById<Button>(R.id.btnPhone)
         val btnSMS = findViewById<Button>(R.id.btnSMS)
         val btnGame = findViewById<Button>(R.id.btnGame)
+        val edtPlayerName = findViewById<EditText>(R.id.editPlayerName)
+
+
+        btnGame.setOnClickListener {
+            startActivity(GameActivity.newIntent(this, edtPlayerName.text.toString()))
+        }
 
         btnPhone.setOnClickListener {
             //Toast.makeText(this, R.string.msgHello, Toast.LENGTH_SHORT).show()
@@ -30,8 +34,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent);
         }
 
-        btnGame.setOnClickListener {
-            startActivity(GameActivity.newIntent(this))
-        }
     }
 }
